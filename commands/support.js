15072a682fd88ce8b9af30afbb9b82a1da3861ac
CVXSL <70546159+CVXSL@users.noreply.github.com>
2022-04-17 17:17:59 -0600

    v1-0-23

--- commands/support.js
+++ commands/support.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
-const update = ("1.0.22")
+const update = ("1.0.23")
 //const update = process.env['UPDATE'];
 const updateInfo = ("• use /inbox for information")
 
