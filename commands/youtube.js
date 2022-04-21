449f3020e0aa376628407e1121a9361bb8d90a08
CVXSL <70546159+CVXSL@users.noreply.github.com>
2022-04-20 21:53:27 -0600

    v1.0.26

--- commands/youtube.js
+++ commands/youtube.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
-const update = ("1.0.24")
+const update = ("1.0.26")
 //const update = process.env['UPDATE'];
 const updateInfo = ("• use /inbox for information")
 
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const update = ("1.0.25")
+const update = ("1.0.26")
 const updateInfo = ("• use /inbox for information")
 
 const fs = require('fs');
